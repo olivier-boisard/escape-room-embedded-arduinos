5913febcef852622eb3e57731d2943d643668dad
olivier-boisard <olivier.boisard@gmail.com>
2022-02-16 08:46:54 +0100

    Formatted code.

--- nfc/000zByteArrayWriter.ino
+++ nfc/000zByteArrayWriter.ino
@@ -3,7 +3,9 @@ class ByteArrayWriter {
     virtual void write(byte outputBuffer[], size_t size) = 0;
     virtual void flush() = 0;
     
-    void write(byte singleByte) { this->write(&singleByte, 1); }
+    void write(byte singleByte) {
+      this->write(&singleByte, 1);
+    }
     
     virtual ~ByteArrayWriter() = default;
 };
