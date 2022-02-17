475d10e91f9f8754dcf13c2b81170ee5340bce14
olivier-boisard <olivier.boisard@gmail.com>
2022-02-17 09:26:55 +0100

    Implementing command processor with common buffer.

--- nfc/100zBoardDriver.ino
+++ nfc/100zBoardDriver.ino
@@ -1,21 +1,9 @@
-bool processHandshakeCommand(byte inputBuffer[], function<void(const byte[], size_t)> writeByteArray) {
-  constexpr byte expectedIncomingCode[] = {0x01, 0xEE, 0x35, 0xD7, 0x2A, 0x80, 0x58, 0xEA};
-  constexpr byte firmwareId[] = {0x85, 0xF2, 0x9E, 0xE3, 0x43, 0x19, 0xEA, 0xF6};
-
-  bool isSuccess = arrayEquals(sizeof(expectedIncomingCode), inputBuffer, expectedIncomingCode);
-  if (isSuccess) {
-    writeByteArray(firmwareId, sizeof(firmwareId));
-  }
-  
-  return isSuccess;
-}
-
 class BoardDriver {
   public:
     BoardDriver(
       const function<size_t(byte[], size_t)>& readByteArray,
       ByteArrayWriter& byteArrayWriter,
-      function<bool(byte[])> processHandshakeCommand
+      function<bool(const byte[], function<void(const byte[], size_t)>&)>& processHandshakeCommand
     ) : readByteArray(readByteArray),
         byteArrayWriter(byteArrayWriter),
         processHandshakeCommand(processHandshakeCommand) {}
@@ -38,9 +26,9 @@ class BoardDriver {
   private:
     function<size_t(byte[], size_t)> readByteArray;
     ByteArrayWriter& byteArrayWriter;
-    function<bool(byte[], function<void(const byte[], size_t)>)> processHandshakeCommand;
+    function<bool(const byte[], function<void(const byte[], size_t)>&)> processHandshakeCommand;
 
-    bool processCommand(byte command, byte args[]) {
+    bool processCommand(byte command, const byte args[]) {
       constexpr byte handshakeCode = 0x10;
       bool isSuccess = false;
       auto wrapper = [this] (byte dataToWrite[], size_t size) { this->byteArrayWriter.write(dataToWrite, size); };
