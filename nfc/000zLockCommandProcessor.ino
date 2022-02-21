8b6c60e30028d258e6db9ef1e1c0f402ada2d812
olivier-boisard <olivier.boisard@gmail.com>
2022-02-21 14:03:50 +0100

    Fixed TODO's.

--- nfc/000zLockCommandProcessor.ino
+++ nfc/000zLockCommandProcessor.ino
@@ -1,14 +1,16 @@
+//TODO factorize with ConfigurationModeCommandProcessor
 class LockCommandProcessor {
   public:
     LockCommandProcessor(const function<bool()>& callback) : callback(callback) {}
-  
+
+    //TODO wrap command and commandSize in struct
     size_t operator()(const byte command[], size_t commandSize, byte* outputBuffer) {
       constexpr byte openStatus = 0x01;
       constexpr byte closedStatus = 0x02;
       constexpr byte toggleCommand = 0x03;
       constexpr byte success = 0xF1;
       constexpr byte errorCode = 0xFF;
-      constexpr byte response[] = {toggleCommand, success};//TODO remove
+      constexpr byte response[] = {toggleCommand, success};
       size_t nWrittenBytes = 0;
       
       if (commandSize == 1 && command[0] == 0x03) {
