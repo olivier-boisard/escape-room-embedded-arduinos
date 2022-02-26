b399f76ffaa68aee8b39efca16567cdabcc8d2ca
olivier-boisard <olivier.boisard@gmail.com>
2022-02-26 15:26:51 +0100

    Make sure we don't load too much data.

--- nfc/100zUidFromEepromReader.ino
+++ nfc/100zUidFromEepromReader.ino
@@ -4,6 +4,10 @@ class UidFromEepromReader {
 
     virtual bool operator()(PiccUid* output) const {
       size_t uidSize = EEPROM.read(address);
+      if (uidSize > PiccUid::MAX_SIZE) {
+        //TODO raise error somehow
+        return false;
+      }
       for (size_t i = 0 ; i < uidSize ; i++) {
         output->value[i] = EEPROM.read(address + i + 1);
       }
