788a6f51814c9c270f2fd302ae31b9681d70a27c
olivier-boisard <olivier.boisard@gmail.com>
2022-03-13 21:39:06 +0100

    Implemented simple I2C slave.

---
+++ I2cRead/I2cRead.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+
+constexpr byte I2C_SLAVE_ADDRESS = 0x01;
+byte lastReceivedByte = 0x00;
+
+void onIc2ReceivedHandler(int) {
+  while (Wire.available()) {
+    lastReceivedByte = Wire.read();
+  }
+}
+
+void onI2cRequestHandler() {
+  Wire.write(lastReceivedByte + 1);
+}
+
+void setup() {
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.onReceive(onIc2ReceivedHandler);
+  Wire.onRequest(onI2cRequestHandler);
+}
+
+void loop() {}
