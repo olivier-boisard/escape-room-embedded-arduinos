1a8c949e4f582234df3cd572392523fd4f534c7e
olivier-boisard <olivier.boisard@gmail.com>
2022-03-31 12:23:19 +0200

    Cleaned up code.

--- I2cRead/I2cRead.ino
+++ I2cRead/I2cRead.ino
@@ -1,22 +1,27 @@
 #include <Wire.h>
 
+#define SDA_PIN 4
+#define SCL_PIN 5
+
 constexpr byte I2C_DEVICE_ADDRESS = 0x08;
 byte lastReceivedByte = 0x00;
 
 void onIc2ReceivedHandler(int) {
   while (Wire.available()) {
     lastReceivedByte = Wire.read();
-    Serial.println("Read byte");
   }
-  Serial.println("Done");
 }
 
 void setup() {
   Serial.begin(9600);
-  Wire.begin(I2C_DEVICE_ADDRESS);
+  Wire.begin(SDA_PIN, SCL_PIN, I2C_DEVICE_ADDRESS);
   Wire.onReceive(onIc2ReceivedHandler);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   delay(1000);
+  Serial.println(lastReceivedByte);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
